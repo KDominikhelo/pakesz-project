056b17fad648813776fc2068018ac365cb7ca851
martinkovacs22 <martinkovacs22@gmail.com>
2023-03-20 19:54:06 +0100

    mindegy már meg csináltam

--- mainDash3/js/main.js
+++ mainDash3/js/main.js
@@ -1,138 +1,147 @@
-        import { Project } from "./modules/project.js"
-        import { Task } from "./modules/task.js"    
-        import { SubTask } from "./modules/subtask.js"    
-        
-        var project = []
+import { Project } from "./modules/project.js"
+import { Task } from "./modules/task.js"    
+import { SubTask } from "./modules/subtask.js"    
 
-        function projectToaddProject(name) {
+var project = []
 
-            project.push(new Project(name));
+function projectToaddProject(name) {
 
+    project.push(new Project(name));
+
+}
+function ProjectToAddTaskByINDEX(name,a) {
+    for (let i = 0; i < project.length; i++) {
+        if (i===a) {
+            project[a].task.push(new Task(name));
         }
-        function ProjectToAddTaskByINDEX(name,a) {
-            for (let i = 0; i < project.length; i++) {
-                if (i===a) {
-                    project.push(new Task(name));
-                }
-            }
+    }
+}
+function TaskToAddSubTaskByINDEX(name,finish,script,a) {
+    
+    for (let i = 0; i < project.length; i++) {
+        if (i===a) {
+            project.push(new SubTask(name,finish,script));
         }
-        function TaskToAddSubTaskByINDEX(name,finish,script,a) {
-            
-            for (let i = 0; i < project.length; i++) {
-                if (i===a) {
-                    project.push(new SubTask(name,finish,script));
-                }
-            }
-        }
-        function getAllProject() {
-            const arr = [];
-            project.map((item)=>{
-                arr.push(item);
-            })
-            return arr;
-        }
-
-        function getAllTaskFromProject(projectIndex) {
-            project[projectIndex].task.map((item)=>{
-                console.log(item.name);
-            })
-        }
-
-        function getAllSubtaskFromTaskFromProject(projectIndex,TaskID) {
-            project[projectIndex].task[TaskID].map((item)=>{
-                console.log(item.name);
-            })
-        }
-
-          const form = document.getElementById("addProject")
-          const projects = document.getElementById("projects");
-        
-          form.addEventListener("submit",(e)=>{
-            e.preventDefault();
-            projects.innerHTML = "";
-            projectToaddProject(e.target.name.value);
-            getAllProject().map((item) =>{
-                projects.innerHTML += `<li><a class="nav-link id="${item.name}id" text-white">${item.name}</a></li>`
-                document.getElementById(`${item.name}id`).addEventListener('click',(e)=>{
-                    e.preventDefault(); 
-                })
-            }); 
-            
-        })
-
-        const task = document.getElementById("task");
-        const taskok = document.getElementById("taskok");
-
-
-        
-
-/*
-            document.getElementById("makeProject").onsubmit = (e)=>{
-                e.preventDefault();
-                    project.push (new Project(e.target.name.value) );
-                    document.getElementById("projects").innerHTML  = "";
-                    project.map((item)=>{
-                        
-                        document.getElementById("projects").innerHTML += `<button id="${item.name}">${item.name}</button>`
-                        
-                        innerProject = document.getElementById("projects");
-                        
-                        
-                    })
-                    for (let i = 0; i < innerProject.childNodes.length; i++) {
-                        innerProject.childNodes[i].onclick = ()=>{
-
-                            document.getElementById("tasks").innerHTML = "";
-                            getfun(i)
-                           document.getElementById("projectupdating").innerHTML = `
-                           
-                           <h1> ${innerProject.childNodes[i].id} </h1>
-
-                           <form id="${innerProject.childNodes[i].id}ID">
-            <input type="text" id="taskName" name="taskName">
-            <input type="area" id="taskAreaName" name="taskAreaName">
-            <input type="submit">
-        </form>
-
-                           `
-
-                           document.getElementById(innerProject.childNodes[i].id+"ID").onsubmit = (e)=>{
-                            e.preventDefault();
-
-                            project.map((item)=>{
-                                if (innerProject.childNodes[i].id == item.name) {
-                                    item.addTask(new Task(e.target.taskName.value,e.target.taskAreaName.value))
-                                    item.getTask().map((item)=>{
-                                        document.getElementById("tasks").innerHTML += item.name
-                                    })
-                                }
-                            })
-
-                        }
-
-                        }
-                        
-                       
-                        
-                        
-                    }
-                }
-       
-function getfun(i) {
+    }
+}
+function getAllProject() {
+    const arr = [];
     project.map((item)=>{
-        if (innerProject.childNodes[i].id == item.name) {
-            //item.addTask(new Task(e.target.taskName.value,e.target.taskAreaName.value))
-            item.getTask().map((item)=>{
-                document.getElementById("tasks").innerHTML += item.name
-            })
-        }
+        arr.push(item);
+    })
+    return arr;
+}
+
+function getAllTaskFromProject(projectIndex) {
+    const arr = [];
+    project[projectIndex].task.map((item)=>{
+        arr.push(item);
+    })
+    return arr;
+}
+
+function getAllSubtaskFromTaskFromProject(projectIndex,TaskID) {
+    project[projectIndex].task[TaskID].map((item)=>{
+        console.log(item.name);
     })
 }
-            */
+
+function showTask(index) {
+    
+  return  getAllTaskFromProject(index).map((element)=>{
+        const task = (item)=>{
+        return `<div class="card mt-4  " draggable="true">
+        <div class="card-header card_post_it">
+          ${item.name}
+        </div>
+        <div class="card-body" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          <h5 class="card-title">${item.name}</h5>
+          <p class="card-text">Itt a leírása a(z) ${item.name} feladatnak.</p>
+        </div>
+      </div>`
+    }
+    document.getElementById("tasks").innerHTML += task(element);
+    })
+}
+
+  const form = document.getElementById("addProject")
+  const projects = document.getElementById("projects");
+
+  form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    projects.innerHTML = "";
+    projectToaddProject(e.target.name.value);
+    getAllProject().map((item,index) =>{
+        projects.innerHTML += `<li><a class="nav-link text-white" id="${item.name}id" >${item.name}</a></li>`
+        console.log(document.getElementById(`${item.name}id`));
+        document.getElementById(`${item.name}id`).addEventListener('click',(e)=>{
+            e.preventDefault();
+            document.getElementById("projectName").innerText = item.name;
+            document.getElementById("tasks").innerHTML = "";
+            document.getElementById("tasks").innerHTML = `<button type="button" id="addTask" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#newTaskModal">Add Task</button>`;
+            document.getElementById("task").addEventListener("submit",(e)=>{
+                e.preventDefault();
+                document.getElementById("tasks").innerHTML = "";
+                document.getElementById("tasks").innerHTML = `<button type="button" id="addTask" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#newTaskModal">Add Task</button>`;
+                
+                    const taskName = e.target.taskName.value;
+                    console.log(index);
+                    ProjectToAddTaskByINDEX(taskName,index);
+                    console.log(getAllTaskFromProject(index))
+                    showTask(index)
+            })
+            showTask(index)
+
+            
            
 
-            
-        
+        })
+    }); 
+    
+})
 
-            
-        
+
+
+
+const columns = document.querySelectorAll(".column");
+
+document.addEventListener("dragstart", (e) => {
+  e.target.classList.add("dragging");
+});
+
+document.addEventListener("dragend", (e) => {
+  e.target.classList.remove("dragging");
+  
+  
+
+});
+
+columns.forEach((item) => {
+  item.addEventListener("dragover", (e) => {
+    const dragging = document.querySelector(".dragging");
+    const applyAfter = getNewPosition(item, e.clientY);
+
+    if (applyAfter) {
+      applyAfter.insertAdjacentElement("afterend", dragging);
+    } else {
+      item.prepend(dragging);
+    }
+  });
+});
+
+function getNewPosition(column, posY) {
+  const cards = column.querySelectorAll(".item:not(.dragging)");
+  let result;
+
+  for (let refer_card of cards) {
+    const box = refer_card.getBoundingClientRect();
+    const boxCenterY = box.y + box.height / 2;
+
+    if (posY >= boxCenterY) result = refer_card;
+  }
+
+  return result;
+}
+
+
+    
--- mainDash3/js/modules/task.js
+++ mainDash3/js/modules/task.js
@@ -1,11 +1,24 @@
 export class Task {
-
+static getAllTask(projectID,projectArray){
+    
+    const arr = null;
+    projectArray.map((item)=>{
+        if (item.ID == projectID) {
+            arr = (item.task)
+        }
+    })
+    return arr;
+}
     constructor (name){
+this.projectID
 this.name = name;
 this.subTask = [];
+this.whereISIT = 0;
     }
 addSubTask(task){
 this.subTask.push(task);
 }
 }
+
+
 
